445432539d1857e23fe212a257e6d45fd999ff47
Cyril Joy <cyril.da.revo@gmail.com>
2024-05-02 16:40:06 -0400

    Add nav bar

--- tasty-react/src/App.tsx
+++ tasty-react/src/App.tsx
@@ -1,17 +1,21 @@
 // import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/home-page'
+import RecipePage from './pages/recipe-page'
+import NavBar from './components/ui/nav-bar'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavBar/>
       <Routes>
         <Route path="/" element={<HomePage/>}/> {/* 👈 Renders at /app/ */}
+        <Route path="/recipies/:recipeId" element={<RecipePage/>}/> 
+        <Route path="*" element={<p>This page does not exist</p>}/> 
       </Routes>
     </>
   )
---
+++ tasty-react/src/pages/recipe-page.tsx
@@ -0,0 +1,11 @@
+
+import { Button } from "@/components/ui/button";
+
+export default function RecipePage(){
+    return(
+        <div>
+            <Button>Hi I'm a Recipe!</Button>
+
+        </div>
+    )
+}
