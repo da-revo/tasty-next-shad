ff47458bf48dd875981c407fb810bc9f664b6be1
Cyril Joy <cyril.da.revo@gmail.com>
2024-05-02 16:53:28 -0400

    Track subdirectory

---
+++ tasty-nextjs/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Kn2cD4rLMdoH7SbbJCIQADzjcMygkansVYH+Gsx9InQ=\"\n}"
---
+++ tasty-nextjs/app/recipies/[recipeId]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Page() {
+  return (
+    <div>
+        Hi! I am a recipe page
+    </div>
+  );
+}
