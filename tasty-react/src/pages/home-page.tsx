be083cef2ec27168607ea3b178c939117d09413b
Cyril Joy <cyril.da.revo@gmail.com>
2024-05-04 14:19:53 -0400

    Implement badge functionality

--- tasty-react/src/pages/home-page.tsx
+++ tasty-react/src/pages/home-page.tsx
@@ -28,17 +28,50 @@ export default function HomePage(){
 
     const [recipes , setRecipes] = useState([]);
 
+    const [filterRecipes , setFilterRecipes] = useState([]);
+
+    const [badge , setBadge] = useState("");
+
+    const getAllRecipes = async () => {
+        const response = await fetch('https://dummyjson.com/recipes');
+        const data = await response.json();
+        return data.recipes;
+    }
+
     useEffect(() => {
         const getRecipes = async () => {
-            const response = await fetch('https://dummyjson.com/recipes');
-            const data = await response.json();
-            if (data){
-                setRecipes(data.recipes);
+            const recipes = await getAllRecipes();
+            if (recipes){
+                setRecipes(recipes);
             }
         };
-
         getRecipes();
     },[])
+
+    useEffect(() => {
+        console.log(badge)
+
+        const getFilteredRecipes = async () => {
+            const recipes = await getAllRecipes();
+            const filteredRecipesByCuisine = recipes.filter(
+                (recipe: RecipeType) => recipe.cuisine === badge
+            );
+            setFilterRecipes(filteredRecipesByCuisine);
+        }
+
+        if(badge) {
+            getFilteredRecipes();
+        }
+        
+    }, [badge]);
+
+    const handleOnClick = (
+        e: React.ChangeEvent<HTMLInputElement>,
+        cuisine: string
+    ) => {
+        e.preventDefault();
+        setBadge(cuisine);
+    }
 
     const cuisines: string[] = [
         "All",
@@ -54,57 +87,62 @@ export default function HomePage(){
     ];
 
     return(
-        <div>
+        <div className="xl:px-24 px-10">
+            <div className="my-12">
 
-            {cuisines.map((cuisine,idx) => (
-                <Badge 
-                    key={`${cuisine}-${idx}`} 
-                    variant={"outline"}
-                    className="border-orange-800 text-gray-900 text-lg
-                    mx-2 my-1 hover:cursor-pointer bg-orange-50 
-                    hover:scale-110 ease-in duration-200"
-                >
-                    {cuisine}
-                </Badge>
-            ))}
-            
-            <div className="grid grid-cols- md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-x-10 gap-y-20 xl:gap-y-32 xl:pt-32 pt-12 pb-40">
-                {recipes.map((item: RecipeType) => (
-                <a href={`/recipes/${item.id}`} key={item.id}>
-                    <Card className="flex flex-col bg-orange-50 hover:scale-105 ease-in duration-200 xl:min-h-[600px] fancyGradient">
-                    <CardHeader>
-                        <img
-                        src={item.image}
-                        alt={item.name}
-                        width={500}
-                        height={500}
-                        className="bg-cover rounded-md shadow-xl"
-                        />
-                    </CardHeader>
-                    <CardContent>
-                        <CardTitle className="uppercase lg:text-3xl relative font-bold line-clamp-2">
-                        {item.name}
-                        </CardTitle>
-                    </CardContent>
-                    <CardFooter className="flex items-start gap-2 lg:gap-12 lg:flex-row flex-col">
-                        <div className="flex flex-col">
-                        <p className="text-lg">Serves</p>
-                        <p className="text-gray-800">{item.servings}</p>
-                        </div>
-                        <div className="flex flex-col">
-                        <p className="text-lg">Prep Time</p>
-                        <p className="text-gray-800">{item.prepTimeMinutes} MIN</p>
-                        </div>
-                        <div className="flex flex-col">
-                        <p className="text-lg">Cook Time</p>
-                        <p className="text-gray-800">{item.cookTimeMinutes} MIN</p>
-                        </div>
-                    </CardFooter>
-                    </Card>
-                </a>
+                {cuisines.map((cuisine,idx) => (
+                    <Badge 
+                        key={`${cuisine}-${idx}`} 
+                        variant={"outline"}
+                        className="border-orange-800 text-gray-900 text-lg
+                        mx-2 my-1 hover:cursor-pointer bg-orange-50 
+                        hover:scale-110 ease-in duration-200"
+                        onClick={(e) => {handleOnClick(e, cuisine)}}
+                    >
+                        {cuisine}
+                    </Badge>
                 ))}
+                
+                <div className="grid grid-cols- md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-x-10 gap-y-20 xl:gap-y-32 xl:pt-32 pt-12 pb-40">
+                    {
+                        ( filterRecipes.length > 0
+                        ? filterRecipes
+                        : recipes).map((item: RecipeType) => (
+                            <a href={`/recipes/${item.id}`} key={item.id}>
+                                <Card className="flex flex-col bg-orange-50 hover:scale-105 ease-in duration-200 xl:min-h-[600px] fancyGradient">
+                                <CardHeader>
+                                    <img
+                                    src={item.image}
+                                    alt={item.name}
+                                    width={500}
+                                    height={500}
+                                    className="bg-cover rounded-md shadow-xl"
+                                    />
+                                </CardHeader>
+                                <CardContent>
+                                    <CardTitle className="uppercase lg:text-3xl relative font-bold line-clamp-2">
+                                    {item.name}
+                                    </CardTitle>
+                                </CardContent>
+                                <CardFooter className="flex items-start gap-2 lg:gap-12 lg:flex-row flex-col">
+                                    <div className="flex flex-col">
+                                    <p className="text-lg">Serves</p>
+                                    <p className="text-gray-800">{item.servings}</p>
+                                    </div>
+                                    <div className="flex flex-col">
+                                    <p className="text-lg">Prep Time</p>
+                                    <p className="text-gray-800">{item.prepTimeMinutes} MIN</p>
+                                    </div>
+                                    <div className="flex flex-col">
+                                    <p className="text-lg">Cook Time</p>
+                                    <p className="text-gray-800">{item.cookTimeMinutes} MIN</p>
+                                    </div>
+                                </CardFooter>
+                                </Card>
+                            </a>
+                    ))}
+                </div>
             </div>
-
         </div>
     )
 }
